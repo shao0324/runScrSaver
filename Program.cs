31ae6f1fa213dbb74815c03c3cc211c237e1f743
Shao-NetLab <terry85324@gmail.com>
2024-10-24 16:39:04 +0800

    first commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+class Program
+{
+    [DllImport("user32.dll")]
+    private static extern void SendMessage(IntPtr hWnd, uint Msg, IntPtr wParam, IntPtr lParam);
+
+    [DllImport("user32.dll", SetLastError = true)]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    private static extern bool LockWorkStation();
+
+    private const uint WM_SYSCOMMAND = 0x0112;
+    private const uint SC_SCREENSAVE = 0xF140;
+
+    static void Main(string[] args)
+    {
+        // 鎖定螢幕
+        LockWorkStation();
+        
+        // 執行螢幕保護程式
+        SendMessage(GetConsoleWindow(), WM_SYSCOMMAND, (IntPtr)SC_SCREENSAVE, IntPtr.Zero);
+    }
+
+    [DllImport("kernel32.dll")]
+    private static extern IntPtr GetConsoleWindow();
+}
