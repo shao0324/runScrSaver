0efb8a29ae20d62d4936610386c43bd25478980f
Shao-NB <terry85324@gmail.com>
2024-10-24 22:20:27 +0800

    clear lagacy code

--- Program.cs
+++ Program.cs
@@ -3,13 +3,6 @@ using System.Runtime.InteropServices;
 
 class Program
 {
-    [DllImport("user32.dll")]
-    private static extern void SendMessage(IntPtr hWnd, uint Msg, IntPtr wParam, IntPtr lParam);
-
-    [DllImport("user32.dll", SetLastError = true)]
-    [return: MarshalAs(UnmanagedType.Bool)]
-    private static extern bool LockWorkStation();
-
     private const uint WM_SYSCOMMAND = 0x0112;
     private const uint SC_SCREENSAVE = 0xF140;
 
@@ -17,11 +10,11 @@ class Program
     {
         // 執行螢幕保護程式
         SendMessage(GetConsoleWindow(), WM_SYSCOMMAND, (IntPtr)SC_SCREENSAVE, IntPtr.Zero);
-
-         // 鎖定螢幕
-        // LockWorkStation();
     }
 
     [DllImport("kernel32.dll")]
     private static extern IntPtr GetConsoleWindow();
+
+    [DllImport("user32.dll", SetLastError = true)]
+    private static extern IntPtr SendMessage(IntPtr hWnd, uint Msg, IntPtr wParam, IntPtr lParam);
 }
